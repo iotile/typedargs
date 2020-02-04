dcc9c49cbe14d38da7df3e97baa34937e589055c
Dimitriy Kutniakov <d.kutnyakov@sirinsoftware.com>
2020-02-04 17:05:40 +0200

    formatter "format_one_line" for basic_dict type

--- typedargs/types/basic_dict.py
+++ typedargs/types/basic_dict.py
@@ -2,6 +2,8 @@
 
 import json
 
+from typedargs.exceptions import ValidationError
+
 
 def convert(arg, **kwargs):
     if arg is None:
@@ -24,3 +26,46 @@ def _json_formatter(arg):
 
 def default_formatter(arg, **kwargs):
     return json.dumps(arg, sort_keys=True, indent=4, separators=(',', ': '), default=_json_formatter)
+
+
+def format_one_line(dict_arg, key_formatter=None, val_formatter=None):
+    """Get string representation of arg
+
+    Args:
+        dict_arg (dict): object of type map
+        key_formatter (callable | string | None): formatter for dict key
+        val_formatter (callable | string | None): formatter for dict value
+
+    Returns:
+        string: string representation of arg
+    """
+    if dict_arg == {}:
+        return str(dict_arg)
+
+    def _get_callable_formatter(obj, formatter):
+        validation_err = ValidationError('Cannot convert to string')
+
+        if formatter is None:
+            return str
+
+        if not callable(formatter):
+            if isinstance(formatter, str):
+                f_name = 'format_{}'.format(formatter)
+                if not (hasattr(obj, f_name) and callable(getattr(obj, f_name))):
+                    raise validation_err
+                formatter = getattr(obj, f_name)
+            else:
+                raise validation_err
+        return formatter
+
+    key = list(dict_arg.keys())[0]
+    val = dict_arg[key]
+    key_formatter = _get_callable_formatter(key, key_formatter)
+    val_formatter = _get_callable_formatter(val, val_formatter)
+
+    str_items = []
+    for key, val in dict_arg.items():
+        key_str = key_formatter(key)
+        val_str = val_formatter(val)
+        str_items.append("{}: {}".format(key_str, val_str))
+    return '{%s}' % ', '.join(str_items)
