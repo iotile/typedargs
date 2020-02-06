3aad35ca9b71cf2e2e48f0dc01bdff8c473b215c
Dimitriy Kutniakov <d.kutnyakov@sirinsoftware.com>
2020-02-06 18:09:34 +0200

    updated type annotations parser

--- typedargs/type_annotations_parser.py
+++ typedargs/type_annotations_parser.py
@@ -1,18 +1,21 @@
 """Routines for extracting parameter and return information from an annotation."""
 from .basic_structures import ParameterInfo, ReturnInfo
+from .typeinfo import TypeSystem
 
 
 def parse_annotations(annotations: dict):
     """Get type info for params and return value from annotations dictionary"""
 
     params = {}
-    returns = ReturnInfo(None, None, None, None)
+    returns = ReturnInfo(None, None, None, None, None)
 
     if 'return' in annotations:
         ret_type = annotations.pop('return')
-        returns = ReturnInfo(ret_type, None, True, None)
+        ret_type_name = TypeSystem.get_type_name(ret_type)
+        returns = ReturnInfo(ret_type, ret_type_name, None, True, None)
 
     for param_name, param_type in annotations.items():
-        params.update({param_name: ParameterInfo(param_type, [], None)})
+        param_type_name = TypeSystem.get_type_name(param_type)
+        params.update({param_name: ParameterInfo(param_type, param_type_name, [], None)})
 
     return params, returns
