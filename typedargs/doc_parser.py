1efc6157f1bdfa5b9298c309b43306e7215d7229
Dimitriy Kutniakov <d.kutnyakov@sirinsoftware.com>
2020-02-07 15:35:35 +0200

    made param type validation optional in parse_param()

--- typedargs/doc_parser.py
+++ typedargs/doc_parser.py
@@ -321,7 +321,7 @@ class ParsedDocstring:
         return out.getvalue()
 
 
-def parse_param(param, include_desc=False):
+def parse_param(param, include_desc=False, validate_type=True):
     """Parse a single typed parameter statement."""
 
     param_def, _colon, desc = param.partition(':')
@@ -335,7 +335,10 @@ def parse_param(param, include_desc=False):
 
     param_name, _space, param_type = param_def.partition(' ')
     if len(param_type) < 2 or param_type[0] != '(' or param_type[-1] != ')':
-        raise ValidationError("Invalid parameter type string not enclosed in ( ) characters", param_string=param_def, type_string=param_type)
+        if validate_type:
+            raise ValidationError("Invalid parameter type string not enclosed in ( ) characters", param_string=param_def, type_string=param_type)
+
+        return param_name, ParameterInfo(None, None, [], desc)
 
     param_type = param_type[1:-1]
     return param_name, ParameterInfo(None, param_type, [], desc)
