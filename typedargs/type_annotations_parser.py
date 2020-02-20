f0e34e966141276f27c64469569608c165670556
Dimitriy Kutniakov <d.kutnyakov@sirinsoftware.com>
2020-02-20 16:51:35 +0200

    brought back type names recognizing for parsing type annotations to support warning log in case of type info mismatch

--- typedargs/type_annotations_parser.py
+++ typedargs/type_annotations_parser.py
@@ -1,7 +1,18 @@
 """Routines for extracting parameter and return information from type annotations."""
 
+import inspect
+import typing
 from typing import Tuple, Dict
 from typedargs.basic_structures import ParameterInfo, ReturnInfo
+
+
+def _get_type_name(type_class):
+    type_name = getattr(type_class, '__name__', None)
+
+    if inspect.getmodule(type_class) == typing and type_name in ('Dict', 'List', 'Tuple'):
+        type_name = type_name.lower()
+
+    return type_name
 
 
 def parse_annotations(annotations: dict) -> Tuple[Dict[str, ParameterInfo], ReturnInfo]:
@@ -12,9 +23,11 @@ def parse_annotations(annotations: dict) -> Tuple[Dict[str, ParameterInfo], Retu
 
     if 'return' in annotations:
         ret_type = annotations.pop('return')
-        returns = ReturnInfo(ret_type, None, None, True, None)
+        type_name = _get_type_name(ret_type)
+        returns = ReturnInfo(ret_type, type_name, None, True, None)
 
     for param_name, param_type in annotations.items():
-        params[param_name] = ParameterInfo(param_type, None, [], None)
+        type_name = _get_type_name(param_type)
+        params[param_name] = ParameterInfo(param_type, type_name, [], None)
 
     return params, returns
