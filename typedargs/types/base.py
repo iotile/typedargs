6716989746d2291d1c44d8be62be02dc0ffc6c6a
Dimitriy Kutniakov <d.kutnyakov@sirinsoftware.com>
2020-02-25 15:18:53 +0200

    define base type class for type classes

---
+++ typedargs/types/base.py
@@ -0,0 +1,16 @@
+
+
+class BaseType:
+
+    @classmethod
+    def FromString(cls, arg):
+        """Create an instance of this class from string."""
+        raise NotImplementedError
+
+    @classmethod
+    def default_formatter(cls, arg):
+        """Convert a given instance of this class to string representation."""
+        raise NotImplementedError
+
+
+
