bb12f2039f8345e7eb0ca5eabfa6a860e1f2bba6
Alexey Kuzmenko <dma@archsys.io>
2021-04-04 16:38:17 -0700

    Add python 3.8 support

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
